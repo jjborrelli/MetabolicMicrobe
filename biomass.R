b4a111441b893dd10cb58c9f132ccfb885f587f0
Jon Borrelli <borrejj@gmail.com>
2017-04-17 16:37:25 -0400

    code to get total biomass at each timestep for single spp run

--- biomass.R
+++ biomass.R
@@ -8,3 +8,12 @@ outp[(1+grep("sparse", outp)[1]):(grep("sparse", outp)[2]-1)]
 outp[(1+grep("sparse", outp)[2]):(grep("sparse", outp)[3]-1)]
 head(outp[(1+grep("sparse", outp)[3]):(grep("sparse", outp)[4]-1)])
 plot(c(1e-7, 9.6130185655e-8, sum(9.2645345082e-8, 4.6626846156e-9, 4.6626846156e-9, 4.6626846156e-9, 4.6626846156e-9)))
+
+test <- (outp[(1+grep("sparse", outp)[3]):(grep("sparse", outp)[4]-1)])
+strsplit(test, "=") %>% sapply(function(x) as.numeric(strsplit(x[2], ";")[1])) %>% sum()
+
+bio1 <- c()
+for(i in 1:(length(grep("sparse", outp))-1)){
+  test <- (outp[(1+grep("sparse", outp)[i]):(grep("sparse", outp)[i+1]-1)])
+  bio1[i] <- strsplit(test, "=") %>% sapply(function(x) as.numeric(strsplit(x[2], ";")[1])) %>% sum()
+}
